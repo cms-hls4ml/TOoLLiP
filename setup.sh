2d6a103584d6af386a66b9ed38a3063c7b862fc1
Javier Duarte <jduarte@ucsd.edu>
2023-12-20 07:02:34 -0800

    update setup

--- setup.sh
+++ setup.sh
@@ -7,3 +7,9 @@ cp    $SRC/myproject.h   TOoLLip_v1/NN/
 cp -r $SRC/nnet_utils   TOoLLip_v1/NN/
 cp    $SRC/parameters.h   TOoLLip_v1/NN/
 cp -r $SRC/weights    TOoLLip_v1/NN/
+
+# change the way the weights are read
+find ./TOoLLip_v1/NN \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/#ifndef __SYNTHESIS__/#ifdef __HLS4ML_LOAD_TXT_WEIGHTS__/'
+# rename project (could be done in hlsm4l as well)
+find ./TOoLLip_v1/NN \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/myproject/TOoLLip_v1/g'
+find ./TOoLLip_v1/NN \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/MYPROJECT_H_/TOOLLIP_V1_H_/g'
