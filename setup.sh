d7f10cc153bb7e7f66ba995fd64e21add1e55718
Daniel Diaz <ddiaz006@ucr.edu>
2023-12-20 16:00:10 +0100

    to edit the weights line

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ cp    $SRC/myproject.h   TOoLLip_v1/NN/
 cp -r $SRC/nnet_utils   TOoLLip_v1/NN/
 cp    $SRC/parameters.h   TOoLLip_v1/NN/
 cp -r $SRC/weights    TOoLLip_v1/NN/
+
+#change the way the wieghts are read
+find ./TOoLLip_v1/NN \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/#ifndef __SYNTHESIS__/#ifdef __HLS4ML_LOAD_TXT_WEIGHTS__/'
